9f562192bc5c1d16afeafa0c48c1d39054c305ed
saiful islam <saifulislam975317@gmail.com>
2023-08-29 02:34:42 +0600

    added my carts

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,6 +1,24 @@
 import { NavLink, Outlet } from "react-router-dom";
+import {
+  AiOutlineShoppingCart,
+  AiFillHome,
+  AiFillCalendar,
+  AiFillCreditCard,
+  AiFillFileAdd,
+  AiTwotoneHome,
+  AiOutlineMenu,
+  AiTwotoneShopping,
+  AiFillContacts,
+  AiFillBook,
+  AiOutlineMenuFold,
+} from "react-icons/ai";
+import { ImSpoonKnife } from "react-icons/im";
+import { FaUsers } from "react-icons/fa";
+import useCarts from "../../hooks/useCarts";
 
 const Dashboard = () => {
+  const isAdmin = false;
+  const [carts] = useCarts();
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -16,13 +34,95 @@ const Dashboard = () => {
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <AiFillHome></AiFillHome>Admin Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/addItem">
+                  <ImSpoonKnife></ImSpoonKnife>Add Items
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                  <AiOutlineMenuFold></AiOutlineMenuFold>Manage Items
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <AiFillBook></AiFillBook>Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/allUsers">
+                  <FaUsers></FaUsers>All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/">
+                  <AiFillHome></AiFillHome>User Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <AiFillCalendar></AiFillCalendar>Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/payment">
+                  <AiFillCreditCard></AiFillCreditCard>Payment history
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myCart">
+                  <AiOutlineShoppingCart></AiOutlineShoppingCart>My Cart
+                  <span className="badge badge-secondary">
+                    +{carts.length || 0}
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  <AiFillFileAdd></AiFillFileAdd>Add Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/booking">
+                  <AiFillCalendar></AiFillCalendar>My Booking
+                </NavLink>
+              </li>
+            </>
+          )}
+          <div className="divider"></div>
           <li>
-            <a>
-              <NavLink to="/dashboard/myCart">My Cart</NavLink>
-            </a>
+            <NavLink to="/">
+              <AiTwotoneHome></AiTwotoneHome>Home
+            </NavLink>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <NavLink to="/menu">
+              <AiOutlineMenu></AiOutlineMenu>Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <AiTwotoneShopping></AiTwotoneShopping>Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/contact">
+              <AiFillContacts></AiFillContacts>Contact
+            </NavLink>
           </li>
         </ul>
       </div>
